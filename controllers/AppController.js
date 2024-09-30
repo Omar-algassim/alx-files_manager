f1fe93128943d784307e8daea8f1cc6fe4ce64f3
Omar-Algassim <omarggg2@gmail.com>
2024-09-30 22:39:47 +0300

    add static to function eslint purpos

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -3,15 +3,14 @@ import dbClient from '../utils/db';
 import redisClient from '../utils/redis';
 
 class AppController {
-
-  getStatus(request, response) {
+  static getStatus(request, response) {
     const redis = redisClient.isAlive();
     const db = dbClient.isAlive();
-    response.status(200).json({ "redis": redis, "db": db });
+    response.status(200).json({ redis, db });
   }
 
-  async getStats(request, response) {
-    const stats = { "users": await dbClient.nbUsers(), "files": await dbClient.nbFiles() };
+  static async getStats(request, response) {
+    const stats = { users: await dbClient.nbUsers(), files: await dbClient.nbFiles() };
     response.status(200).json(stats);
   }
 }
