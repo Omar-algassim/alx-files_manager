b1fb24205d49647aa9cd744eed5d16eaec434431
Omar-Algassim <omarggg2@gmail.com>
2024-09-30 22:55:57 +0300

    fix eslint

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -3,16 +3,20 @@ import dbClient from '../utils/db';
 import redisClient from '../utils/redis';
 
 class AppController {
+  constructor() {
+    this.getStatus = this.getStatus.bind(this);
+    this.getStats = this.getStats.bind(this);
+      }
 
-  getStatus(request, response) {
+  static getStatus(request, response) {
     const redis = redisClient.isAlive();
     const db = dbClient.isAlive();
-    response.status(200).json({ "redis": redis, "db": db });
+    response.status(200).json({ redis, db });
   }
 
-  async getStats(request, response) {
-    const stats = { "users": await dbClient.nbUsers(), "files": await dbClient.nbFiles() };
-    response.status(200).json(stats);
+  static getStats(request, response) {
+    const [user, file] = Promise.all([dbClient.nbUsers(), dbClient.nbFiles()]);
+    response.status(200).json({ users: user, files: file });
   }
 }
 
