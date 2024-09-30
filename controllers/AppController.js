5bfe2ca41bd9532e79e7b98534fd68219dbd803d
Omar-Algassim <omarggg2@gmail.com>
2024-09-30 23:21:04 +0300

    Revert "Revert "add static to function eslint purpos""
    This reverts commit 730c0ea5aae031f779f2285381fb17da514b0f04.

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -9,11 +9,9 @@ class AppController {
     response.status(200).json({ redis, db });
   }
 
-  static getStats(request, response) {
-    Promise.all([dbClient.nbUsers(), dbClient.nbFiles()])
-      .then(([user, file]) => {
-        response.status(200).json({ users: user, files: file });
-      });
+  async getStats(request, response) {
+    const stats = { "users": await dbClient.nbUsers(), "files": await dbClient.nbFiles() };
+    response.status(200).json(stats);
   }
 }
 
