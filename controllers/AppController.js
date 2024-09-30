34167b0bc8bf0af57bd54520efde2dca36574627
Omar-Algassim <omarggg2@gmail.com>
2024-10-01 01:42:27 +0300

    trmove static

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -3,7 +3,7 @@ import dbClient from '../utils/db';
 import redisClient from '../utils/redis';
 
 class AppController {
-  static getStatus(request, response) {
+  getStatus(request, response) {
     const redis = redisClient.isAlive();
     const db = dbClient.isAlive();
     response.status(200).json({ redis, db });
